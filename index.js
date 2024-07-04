3aa0ca961fa44cc1f2db2cfb7750a241ea85c8d6
Shawn McKee <shawnroosemckee@gmail.com>
2024-07-04 10:56:33 -0400

    added remove functionality, styled buttons

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Have to use type=module on the script tag in index.html for this to work(import)
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js'
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
+import { getDatabase, ref, push, onValue, remove } from 'https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js'
 
 
 const appSettings = {    // This is an object which holds the url of the database
@@ -22,20 +22,49 @@ addButtonEl.addEventListener('click', function(){
     push(itemsInDB, inputValue)     
 
     clearInputField()
-
 })
 
-//list the database items on the screen
+
+
+//list the database items on the screen; onVlue is constantly updating the information
 onValue(itemsInDB, function(snapshot){
-    let itemsArray = Object.values(snapshot.val())  //convert on=bject into an array
-    clearShoppingListEl()
-    for(let i = 0; i < itemsArray.length; i++) {
-        addItemToList(itemsArray[i])
+
+    if (snapshot.exists()){     //This checks to make sure that there is not a null in the DB, which would throw an error
+
+        let itemsArray = Object.entries(snapshot.val())  //convert object into an array; getting the values
+
+
+        clearShoppingListEl()
+
+        for(let i = 0; i < itemsArray.length; i++) {
+            let currentItem = itemsArray[i]
+
+            addItemToList(currentItem)
+        }
+    } else {
+        shoppingListEl.innerHTML = 'No Items Here Yet!'
     }
 })
 
- function addItemToList(value) {
-    shoppingListEl.innerHTML += `<li>${value}</li>`
+ function addItemToList(item) {
+    // shoppingListEl.innerHTML += `<li>${value}</li>`
+
+    let itemID = item[0]
+    let itemValue = item[1]
+
+    let newEl = document.createElement("li")    //creating the list el
+    newEl.textContent = itemValue                   //having the text content equal the value 
+
+    shoppingListEl.append(newEl)                //append newEl to the parent el, shoppingListEl(the<ul>)
+
+    // Remove the item from the screen and DB
+    newEl.addEventListener('dblclick', function(){
+        let locationOfItemsInDB = ref(database, `items/${itemID}`) //must have precise path to db
+
+        remove(locationOfItemsInDB)
+    })
+
+
  }
 
  function clearInputField() {
